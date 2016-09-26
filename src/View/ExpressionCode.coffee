cdglabs/apparatus

commit 0fb273c5aa4ee35692c0e74b55dc5d6f72a3d29c

Switch to using ids as referenceKeys, so copy-paste works on references (#63)

src/View/ExpressionCode.coffee
@@ -15,7 +15,7 @@ R.create "ExpressionCode",
 
   contextTypes:
     editor: Model.Editor # extraneous
-    project: Model.Project # extraneous
+    project: Model.Project
     dragManager: R.DragManager
     hoverManager: R.HoverManager # extraneous
     # Note: we need to include all the context variables to pass down to
@@ -108,17 +108,15 @@ R.create "ExpressionCode",
     @_updateMarks()
 
   _updateAttributeFromMirror: ->
+    project = @context.project
     attribute = @props.attribute
     newExprString = @mirror.getValue()
     if attribute.exprString != newExprString
-      # We clean up references to get rid of attribute references which no
-      # longer appear in exprString.
-      oldReferences = attribute.references()
       newReferences = {}
-      for own referenceKey, referenceNode of oldReferences
-        isUsed = (newExprString.indexOf(referenceKey) != -1)
+      for own id, node of project.editingElementNodesById()
+        isUsed = (newExprString.indexOf(id) != -1)
         if isUsed
-          newReferences[referenceKey] = referenceNode
+          newReferences[id] = node
       attribute.setExpression(newExprString, newReferences)
 
 
@@ -135,7 +133,7 @@ R.create "ExpressionCode",
   _replaceSelectionWithReference: (referenceAttribute) ->
     {attribute} = @props
     references = attribute.references()
-    referenceKey = Util.generateId()
+    referenceKey = Util.getId(referenceAttribute)
     references[referenceKey] = referenceAttribute
     exprString = attribute.exprString
     attribute.setExpression(exprString, references)
@@ -144,7 +142,7 @@ R.create "ExpressionCode",
   _replaceAllWithReference: (referenceAttribute) ->
     {attribute} = @props
     references = {}
-    referenceKey = Util.generateId()
+    referenceKey = Util.getId(referenceAttribute)
     references[referenceKey] = referenceAttribute
     exprString = referenceKey
     attribute.setExpression(exprString, references)
@@ -168,18 +166,19 @@ R.create "ExpressionCode",
     value = @mirror.getValue()
     marks = []
     for own referenceKey, referenceAttribute of attribute.references()
-      startChar = value.indexOf(referenceKey)
-      continue if startChar == -1
-      endChar = startChar + referenceKey.length
-      do (referenceAttribute) ->
-        from = Util.charToLineCh(value, startChar)
-        to = Util.charToLineCh(value, endChar)
-        render = ->
-          R.AttributeToken {
-            attribute: referenceAttribute
-            contextElement: attribute.parentElement()
-          }
-        marks.push {from, to, render}
+      startChar = -1
+      while (startChar = value.indexOf(referenceKey, startChar)) > -1
+        endChar = startChar + referenceKey.length
+        do (referenceAttribute) ->
+          from = Util.charToLineCh(value, startChar)
+          to = Util.charToLineCh(value, endChar)
+          render = ->
+            R.AttributeToken {
+              attribute: referenceAttribute
+              contextElement: attribute.parentElement()
+            }
+          marks.push {from, to, render}
+        startChar = endChar
     return marks
 
   _updateMarks: ->

src/Model/Editor.coffee
@@ -63,6 +63,7 @@ module.exports = class Editor
     # and convert or fail appropriately.
     if json.type == "Apparatus"
       @project = @serializer.dejsonify(json)
+      @project.performIdempotentCompatibilityFixes()
 
   loadJsonStringIntoProjectFromExternalSource: (jsonString) ->
     @loadJsonStringIntoProject(jsonString)

src/Model/Project.coffee
@@ -1,6 +1,7 @@
 _ = require "underscore"
 Model = require "./Model"
 Dataflow = require "../Dataflow/Dataflow"
+Util = require "../Util/Util"
 NodeVisitor = require "../Util/NodeVisitor"
 
 
@@ -23,6 +24,7 @@ module.exports = class Project
       "controlledAttributes"
       "implicitlyControlledAttributes"
       "controllableAttributes"
+      "editingElementNodesById"
     ]
     for prop in propsToMemoize
       this[prop] = Dataflow.memoize(this[prop].bind(this))
@@ -104,12 +106,16 @@ module.exports = class Project
     index = @createPanelElements.indexOf(@editingElement)
     @createPanelElements.splice(index, 0, master)
 
-  findUnnecessaryNodes: ->
-    # These nodes are necessary per se.
+  rootNodes: ->
     rootNodes = @createPanelElements.slice()
     for name, obj of Model
       if Model.Node.isPrototypeOf(obj)
         rootNodes.push(obj)
+    return rootNodes
+
+  findUnnecessaryNodes: ->
+    # These nodes are necessary per se.
+    rootNodes = @rootNodes()
 
     unnecessaryNodes = []
 
@@ -144,3 +150,51 @@ module.exports = class Project
 
   controllableAttributes: ->
     return @selectedParticularElement?.element.controllableAttributes() ? []
+
+  editingElementNodesById: ->
+    nodesById = {}
+
+    nodeVisitor = new NodeVisitor
+      linksToFollow: {master: yes, variants: no, parent: no, children: yes}
+      onVisit: (node) ->
+        nodesById[Util.getId(node)] = node
+    nodeVisitor.visit(@editingElement)
+    nodeVisitor.finish()
+
+    return nodesById
+
+
+  # ===========================================================================
+  # Compatibility fixes
+  # ===========================================================================
+
+  # Whenever a project is loaded from a saved copy,
+  # `performIdempotentCompatibilityFixes` is run on it. If you want to make a
+  # breaking change to the format of a project, but you can come up with a way
+  # to update old projects, put it in here.
+  performIdempotentCompatibilityFixes: ->
+    @switchExpressionsToUseNodeIdsAsReferences()
+
+  # See https://github.com/cdglabs/apparatus/pull/63
+  switchExpressionsToUseNodeIdsAsReferences: ->
+    rootNodes = @rootNodes()
+
+    nodeVisitor = new NodeVisitor
+      linksToFollow: {master: yes, variants: no, parent: no, children: yes}
+      onVisit: (node) ->
+        if node.isVariantOf(Model.Attribute) and node.isNovel()
+          exprString = node.exprString
+          references = node.references()
+
+          newExprString = exprString
+          newReferences = {}
+
+          _.each references, (reference, key) ->
+            id = Util.getId(reference)
+            newExprString = newExprString.replace(new RegExp(key, "g"), id)
+            newReferences[id] = reference
+
+          node.setExpression(newExprString, newReferences)
+
+    nodeVisitor.visit(rootNode) for rootNode in rootNodes
+    nodeVisitor.finish()

src/View/AttributeRow.coffee
@@ -91,6 +91,7 @@ R.create "AttributeLabel",
         isHovered: hoverManager.hoveredAttribute == attribute
         isGoingToChange: _.contains(hoverManager.attributesToChange, attribute)
       }
+      title: Util.getId(attribute)
       onMouseDown: @_onMouseDown
       onMouseEnter: @_onMouseEnter
       onMouseLeave: @_onMouseLeave

commit eab60becd2c02c9fc520c5a300d87b324e5e67f1

Only look for attribute IDs in expressions
Currently, built-in nodes have human-readable IDs like "Node", so
0fb273c caused crashes when these human-readable IDs occurred in
expression strings. Since no /attributes/ have human-readable IDs,
this change is all that is needed to prevent this problem.

src/View/ExpressionCode.coffee
@@ -114,9 +114,10 @@ R.create "ExpressionCode",
     if attribute.exprString != newExprString
       newReferences = {}
       for own id, node of project.editingElementNodesById()
-        isUsed = (newExprString.indexOf(id) != -1)
-        if isUsed
-          newReferences[id] = node
+        if node.isVariantOf(Model.Attribute)
+          isUsed = (newExprString.indexOf(id) != -1)
+          if isUsed
+            newReferences[id] = node
       attribute.setExpression(newExprString, newReferences)
 
 