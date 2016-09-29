cdglabs/apparatus

commit dac7312f9c7ef55912ac18222c2f8b847e9bda4d

Generic flexbox layout classes (+ assorted cleanup)
My hypothesis is that, as far as app UI layout is concerned, the
HTML/CSS "separation of concerns" is absurd. Rather than scatter
`flex` properties throughout our CSS to specify layout behavior, let's
design a set of reusable layout primitives which we can refer to
directly in our HTML. So far, it looks nice.

src/View/AttributeRow.coffee
@@ -32,6 +32,7 @@ R.create "AttributeRow",
     attributeRow = R.div {
       className: R.cx {
         AttributeRow: true
+        FlexRow: true
         isInherited: !attribute.isNovel()
         isWrapped: @_isWrapped()
         isGoingToChange: @_isGoingToChange()
@@ -54,7 +55,7 @@ R.create "AttributeRow",
         }
       R.div {className: "AttributeRowLabel"},
         R.AttributeLabel {attribute}
-      R.div {className: "AttributeRowExpression"},
+      R.div {className: "AttributeRowExpression FlexGrow"},
         R.Expression {attribute}
       R.div {
         className: "AttributeRowDragHandle icon-grip"

style/index.styl
@@ -2,6 +2,12 @@
 @import "../node_modules/codemirror/addon/hint/show-hint.css"
 
 
+// Please keep this code well-commented. If you add a property to fix some edge
+// case or browser quirk, then explain this in a comment. Otherwise, you risk
+// the possibility that a future contributor will remove the property, thinking
+// it is no longer necessary.
+
+
 // ============================================================================
 // Mixins
 // ============================================================================
@@ -43,7 +49,7 @@ controlledColor = #c00
 
 html, body
   margin 0
-  overflow hidden
+  overflow hidden  // prevents "overscroll" of the app
 
 [contenteditable]:active, [contenteditable]:focus, button:active, button:focus
   outline none
@@ -62,6 +68,25 @@ canvas
 
 .CursorOverride *
   cursor inherit !important
+
+.FlexRow
+  display flex
+  flex-direction row
+
+.FlexColumn
+  display flex
+  flex-direction column
+
+.FlexGrow
+  flex 1
+
+.FlexNoShrink
+  flex 0 0 auto
+
+// Strictly speaking, this is the same as FlexRow, but it communicates "I'm
+// just here to hold a single child; don't think of me as a row".
+.FlexContainer
+  display flex
 
 
 // ============================================================================
@@ -89,31 +114,21 @@ RightResizeWidth = 10px
 
 .Editor
   position absolute
-  display flex
   width 100%
   height 100%
 
 .Center
-  flex-grow 2
-  display flex
-  flex-direction column
-  height 100%
+  overflow-x hidden  // so it can shrink smaller than the menu bar's width
 
 .CreatePanel
   position relative
-  flex-shrink 0
   box-sizing border-box
-  top 0
-  left 0
-  bottom 0
   width CreatePanelWidth
   background-color #eee
   border-right 1px solid #ccc
 
 .RightPanel
   position relative
-  // this panel should not shrink
-  flex-shrink 0
   box-sizing border-box
 
 .CreatePanelContainer, .RightPanelContainer
@@ -155,23 +170,14 @@ RightResizeWidth = 10px
 
 .BareEditorCanvas, .BareViewerCanvas
   position relative
-  flex-basis 100%
-
   canvas
     position absolute
 
 .EditorCanvasDropzone
-  flex-grow 2
-
-  display flex
-  flex-direction row
-
   &.dropActive
     background-color #F8FFF8
-
   &.dropReject
     background-color #FFF8F8
-
 
 .LayoutMode
   noSelect()
@@ -193,11 +199,6 @@ RightResizeWidth = 10px
   &.FullScreen
     top 2px
 
-.Menubar
-  box-sizing border-box
-  height MenubarHeight
-  background-color #eee
-  border-bottom 1px #ccc solid
 
 // ============================================================================
 // Layout "Chrome"
@@ -234,6 +235,7 @@ HeaderSize = 18px
   overflow-y auto
   overflow-x hidden
 
+
 // ============================================================================
 // Menubar
 // ============================================================================
@@ -241,8 +243,13 @@ HeaderSize = 18px
 .Menubar
   user-select none
   padding 4px
-  display flex
+  box-sizing border-box
+  height MenubarHeight
+  background-color #eee
+  border-bottom 1px #ccc solid
+
   .MenubarItem
+    white-space nowrap
     padding 0 8px
     cursor pointer
     &:hover
@@ -252,11 +259,9 @@ HeaderSize = 18px
       color #aaa
       &:hover
         color #aaa
+
   .MenubarSeparator
     width 24px
-  .ToolbarRight
-    flex-grow 1
-    text-align right
 
 
 // ============================================================================
@@ -328,12 +333,9 @@ HeaderSize = 18px
 
 
 .ElementRow
-  display flex
   padding: 0.175em 0
   .ElementRowDisclosure
     width 1.5em
-  .ElementRowLabel
-    flex-grow 1
 
 
 .DisclosureTriangle
@@ -403,8 +405,7 @@ HeaderSize = 18px
 // ============================================================================
 
 .AttributeRow
-  padding: 0.175em 0
-  display flex
+  padding 0.175em 0
 
   &.isInherited
     opacity 0.5
@@ -419,9 +420,6 @@ HeaderSize = 18px
   .AttributeRowLabel
     forceWidth(9em)
 
-  .AttributeRowExpression
-    flex-grow 1
-
   .AttributeRowDragHandle
     text-align right
     min-width 25px
@@ -430,9 +428,6 @@ HeaderSize = 18px
 
   &.isWrapped
     flex-wrap wrap
-    .AttributeRowExpression
-      padding-left 3.25em
-      width: 100%
     .AttributeRowControl
       order 1
     .AttributeRowLabel
@@ -442,6 +437,8 @@ HeaderSize = 18px
       flex-grow 1
     .AttributeRowExpression
       order 4
+      padding-left 3.25em
+      width: 100%
 
   .AttributeControl
     width 1em

src/Model/Layout.coffee
@@ -6,10 +6,10 @@ module.exports = class Layout
     @showAttributesInOutline = true
 
     @_rightPanelMin = 100
-    @_rightPanelMax = 600
+    @_rightPanelMax = 700
 
-  # applies the constraints to the new right panel width
-  constraintRightPanelWidth: (newWidth) ->
+  # Note: constraints will be applied before setting the new right-panel width
+  setRightPanelWidth: (newWidth) ->
     @rightPanelWidth = Math.min(@_rightPanelMax, Math.max(@_rightPanelMin, newWidth))
     @_refreshLayout()
 

src/View/ApparatusCanvas.coffee
@@ -516,25 +516,26 @@ R.create "EditorCanvas",
         onClick: @_toggleLayout
       }
 
-    if layout.fullScreen
-      R.BareViewerCanvas {
-        element: editingElement
-      },
-        fullScreenButton
-    else
-      R.Dropzone {
-        className: "EditorCanvasDropzone"
-        onDrop: @_onFilesDrop
-        disableClick: true
-        activeClassName: "dropActive"
-        rejectClassName: "dropReject"
-        accept: "image/*"
-        multiple: false
-      },
-        R.BareEditorCanvas {
+    R.div {className: "EditorCanvas FlexGrow FlexContainer"},
+      if layout.fullScreen
+        R.BareViewerCanvas {
           element: editingElement
         },
           fullScreenButton
+      else
+        R.Dropzone {
+          className: "EditorCanvasDropzone FlexGrow FlexContainer"
+          onDrop: @_onFilesDrop
+          disableClick: true
+          activeClassName: "dropActive"
+          rejectClassName: "dropReject"
+          accept: "image/*"
+          multiple: false
+        },
+          R.BareEditorCanvas {
+            element: editingElement
+          },
+            fullScreenButton
 
   _toggleLayout: ->
     {layout} = @context.editor
@@ -571,7 +572,7 @@ R.create "EditorCanvas",
 R.create "BareEditorCanvas",
   render: ->
     R.ApparatusCanvas {
-      className: "BareEditorCanvas"
+      className: "BareEditorCanvas FlexGrow"
       element: @props.element
       cacheRect: true
       screenMatrixScale: 1
@@ -592,7 +593,7 @@ R.create "BareEditorCanvas",
 R.create "BareViewerCanvas",
   render: ->
     R.ApparatusCanvas {
-      className: "BareViewerCanvas"
+      className: "BareViewerCanvas FlexGrow"
       element: @props.element
       cacheRect: false
       screenMatrixScale: 1
@@ -613,7 +614,7 @@ R.create "BareViewerCanvas",
 R.create "ThumbnailCanvas",
   render: ->
     R.ApparatusCanvas {
-      className: "ThumbnailCanvas"
+      className: "ThumbnailCanvas FlexGrow"
       element: @props.element
       cacheRect: true  # no interaction so it's just for size
       screenMatrixScale: 0.1

src/View/CreatePanel.coffee
@@ -16,7 +16,7 @@ R.create "CreatePanel",
     if layout.fullScreen
       return null
 
-    R.div { className: "CreatePanel" },
+    R.div { className: "CreatePanel FlexNoShrink" },
       R.div { className: "CreatePanelContainer" },
         R.div {className: "Header"}, "Symbols"
         R.div {className: "Scroller"},

src/View/Editor.coffee
@@ -19,10 +19,10 @@ R.create "Editor",
 
   render: ->
     R.MouseManagersWrapper {},
-      R.div { className: "Editor" },
+      R.div { className: "Editor FlexRow" },
         R.DragHint {}
         R.CreatePanel {}
-        R.div { className: "Center" },
+        R.div { className: "Center FlexGrow FlexColumn" },
           R.Menubar {}
           R.EditorCanvas {}
         R.RightPanel {}

src/View/Menubar.coffee
@@ -16,7 +16,7 @@ R.create "Menubar",
     if editor.layout.fullScreen
       return null
 
-    R.div { className: "Menubar" },
+    R.div { className: "Menubar FlexRow" },
       R.MenubarItem {title: "New", isDisabled: false, fn: @_new}
       R.MenubarItem {title: "Load", isDisabled: false, fn: @_load}
       if editor.experimental

src/View/Outline.coffee
@@ -16,14 +16,15 @@ R.create "Outline",
     R.div {className: "Outline"},
       R.div {className: "Header"},
         "Outline"
-        R.span {
-          className: "HeaderSetting"
-          onClick: -> layout.toggleShowAttributesInOutline()
-        },
-          if layout.showAttributesInOutline
-            "Hide Attributes"
-          else
-            "Show Attributes"
+        if layout.rightPanelWidth > 200
+          R.span {
+            className: "HeaderSetting"
+            onClick: -> layout.toggleShowAttributesInOutline()
+          },
+            if layout.showAttributesInOutline
+              "Hide Attributes"
+            else
+              "Show Attributes"
       R.div {className: "Scroller"},
         R.OutlineTree {element}
 
@@ -119,7 +120,7 @@ R.create "OutlineItem",
       onMouseEnter: @_onMouseEnter
       onMouseLeave: @_onMouseLeave
     },
-      R.div {className: "ElementRow"},
+      R.div {className: "ElementRow FlexRow"},
         R.div {className: "ElementRowDisclosure"},
           R.div {
             className: R.cx {

commit fb5cc055cb983da51a91f0d80bac8af764a8a538

Fix issues with multi-line expressions caused by dac7312

src/View/AttributeRow.coffee
@@ -58,11 +58,14 @@ R.create "AttributeRow",
       R.div {className: "AttributeRowExpression FlexGrow"},
         R.Expression {attribute}
       R.div {
-        className: "AttributeRowDragHandle icon-grip"
-        onMouseDown: @_onDragHandleMouseDown
+        className: "AttributeRowDragHandleSpacer"
         style:
           visibility: if canDrag and @hovered then "visible" else "hidden"
-      }
+      },
+        R.span {
+          className: "AttributeRowDragHandle icon-grip"
+          onMouseDown: @_onDragHandleMouseDown
+        }
 
     if isDropSpot
       R.div {},

style/index.styl
@@ -420,10 +420,13 @@ HeaderSize = 18px
   .AttributeRowLabel
     forceWidth(9em)
 
-  .AttributeRowDragHandle
+  .AttributeRowDragHandleSpacer
     text-align right
-    min-width 25px
     margin 0px 4px
+
+  .AttributeRowDragHandle
+    display inline-block
+    min-width 20px
     cursor grab
 
   &.isWrapped
@@ -432,13 +435,13 @@ HeaderSize = 18px
       order 1
     .AttributeRowLabel
       order 2
-    .AttributeRowDragHandle
+    .AttributeRowDragHandleSpacer
       order 3
       flex-grow 1
     .AttributeRowExpression
       order 4
       padding-left 3.25em
-      width: 100%
+      flex-basis 100%
 
   .AttributeControl
     width 1em